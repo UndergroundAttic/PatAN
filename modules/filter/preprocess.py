2e3501f30aa2fbe7c66e0d155db6a57a2f59598c
UndergroundAttic <parkinglot0326@gmail.com>
2025-12-03 14:26:27 +0900

    preprocessor fixed;

--- modules/filter/preprocess.py
+++ modules/filter/preprocess.py
@@ -28,9 +28,7 @@ STOP_TAGS = {
 }
 
 
-def preprocess_text(
-    text, tagger_instance: Union[Okt, Hannanum, Komoran], join: bool = True
-) -> list[str]:
+def preprocess_text(text, tagger_instance: Union[Okt, Hannanum, Komoran]) -> list[str]:
     """
     전처리 함수: 텍스트를 입력받아 명사 추출 및 필터링을 수행합니다.
     """
@@ -49,29 +47,25 @@ def preprocess_text(
 
     # 필터링: 한 글자 단어 제거, 불용어 제거, 숫자만 있는 단어 제거, 순수 영문자 제거
 
-    taggedset = set(tagged)
+    filtered_words = [
+        "/".join((word, tag))
+        for word, tag in tagged
+        if (tag not in STOP_TAGS) and (word not in PATENT_STOPWORDS) and len(word) > 1
+    ]
+
+    taggedfilterset = set(filtered_words)
 
     def _helper(item):
         try:
-            taggedset.remove(item)
+            taggedfilterset.remove(item)
         except:
             return False
         return True
 
-    unique_filtered_words = [
-        item
-        for item in [
-            "/".join((word, tag))
-            for word, tag in tagged
-            if (tag not in STOP_TAGS)
-            and (word not in PATENT_STOPWORDS)
-            and len(word) > 1
-        ]
-        if _helper(item)
-    ]
+    unique_filtered_words = [item for item in filtered_words if _helper(item)]
     # TODO: 조사 제거, 일정 숫자마다 프린트
 
-    return unique_filtered_words if not join else " ".join(unique_filtered_words)
+    return unique_filtered_words
 
 
 @elapsed
@@ -99,14 +93,14 @@ def preprocess_df(
     def _wrap(*args):
         t, a, tw, aw = args
         return " ".join(
-            aw * preprocess_text(a, tagger_instance),
-            tw * preprocess_text(t, tagger_instance),
+            aw * preprocess_text(a, tagger_instance)
+            + tw * preprocess_text(t, tagger_instance)
         )
 
-    df["processed_text"] = [
-        _wrap(title, abstract, title_weight, abstract_weight)
-        for title, abstract in zip(df["title"], df["abstract"])
-    ]
+    df["processed_text"] = df.apply(
+        lambda row: _wrap(row["title"], row["abstract"], title_weight, abstract_weight),
+        axis=1,
+    )
 
     return df
 
